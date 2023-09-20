0faa431461d202b173065eeb9799aae09ed956bd
sadik312 <sadikislam46@gmail.com>
2023-09-20 09:51:17 +0100

    created constructor, getter and setter

--- GradeBookApp/GradeBook.cs
+++ GradeBookApp/GradeBook.cs
@@ -1,7 +1,20 @@
 namespace GradeBookApp
 {
-    public class GradeBookApp
+    public class GradeBook
     {
+        private string _name; // common practive to denote private var with _ in front
 
+        public GradeBook(string name) // constructor
+        {
+            _name = name;
+        }
+
+        public string Name
+        {
+            // getter
+            get { return _name; }
+            // setter
+            set { _name = value; }
+        }
     }
 }
---
+++ GradeBookApp/README.md
@@ -0,0 +1,2 @@
+How to create dotnet app:
+- `dotnet new console -n <name of app>`
