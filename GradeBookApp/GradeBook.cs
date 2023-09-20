a35250defd895c6269e03248ad8c8cb0b26ab83b
sadik312 <sadikislam46@gmail.com>
2023-09-20 09:57:23 +0100

    added AddGrade and GetAverageGrade methods

--- GradeBookApp/GradeBook.cs
+++ GradeBookApp/GradeBook.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
 namespace GradeBookApp
 {
     public class GradeBook
     {
         private string _name; // common practive to denote private var with _ in front
+        private List<double> grade;
 
         public GradeBook(string name) // constructor
         {
@@ -15,6 +19,21 @@ namespace GradeBookApp
             get { return _name; }
             // setter
             set { _name = value; }
+        }
+
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        public double GetAverageGrade()
+        {
+            double sum = 0.0;
+            foreach(double grade in grades)
+            {
+                sum += grade;
+            }
+
         }
     }
 }
