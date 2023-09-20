2591958d027d76d59dd83302ca3ac460493a0483
sadik312 <sadikislam46@gmail.com>
2023-09-20 11:22:10 +0100

    created readonly method

--- shopmvc/Controllers/ProductController.cs
+++ shopmvc/Controllers/ProductController.cs
@@ -6,9 +6,19 @@ using Microsoft.AspNetCore.Mvc;
 namespace shopmvc.Controllers;
 public class ProductConroller : Controller
 {
+    
+    private readonly ProductDbContext  _db;
+
+    // make controller
+    // called 'dependency injection'
+    public ProductController(ProductDbContext db)
+    {
+        _db = db;
+    }
     public IActionResult Products()
     {
         // code that we want to execute when uer visits product/products
         IEnumerable<ProductModel> productList = _db.Product.ToList();
+        return View(productList);
     }
 }
