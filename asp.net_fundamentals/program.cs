0f3127945d73a73831cf506d002f65da830fd747
sadik312 <sadikislam46@gmail.com>
2023-10-08 22:17:34 +0100

    added c# program skeleton and explained namespaces in program.cs

---
+++ asp.net_fundamentals/program.cs
@@ -0,0 +1,40 @@
+// A skeleton of a C# program
+using System;
+
+// Your program starts here:
+Console.WriteLine("Hello world!");
+
+namespace YourNamespace
+{
+    class YourClass
+    {
+    }
+
+    struct YourStruct
+    {
+    }
+
+    interface IYourInterface
+    {
+    }
+
+    delegate int YourDelegate();
+
+    enum YourEnum
+    {
+    }
+
+    namespace YourNestedNamespace
+    {
+        struct YourStruct
+        {
+        }
+    }
+}
+
+/*
+Each program contains 0 or more namespaces
+Namespaces contain classes, structs, interfaces, enumerations, delegates or other namespaces
+This is done to organise the classes so that they are easy to handle
+If there are 2 classes with the same name in different namespaces, they will not conflict
+*/
