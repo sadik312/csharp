31cdf31ab1ea8615cd2ff2a1c5e660e31ac9890e
sadik312 <sadikislam46@gmail.com>
2023-10-09 23:28:46 +0100

    created Animal interface

--- asp.net_fundamentals/interfaces.cs
+++ asp.net_fundamentals/interfaces.cs
@@ -1,2 +1,8 @@
 // Interfaces: 
 // abstract class which can only contain abstract methods and properties with empty bodies
+
+interface Animal
+{
+    void animalSound(); // inerface method (does not have a body)
+    void run(); // interface method (does not have a body)
+}
