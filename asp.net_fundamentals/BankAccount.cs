30ac7b18f481569ecdbba37080094f77b075ee84
sadik312 <sadikislam46@gmail.com>
2023-10-10 23:11:53 +0100

    creating bank app

---
+++ asp.net_fundamentals/BankAccount.cs
@@ -0,0 +1,18 @@
+namespace Classes;
+
+public class BankAccount
+{
+    public string Number { get; set; }
+    public string Owner { get; set; }
+    public decimal Balance { get; set; }
+
+    public void MakeDeposit(decimal amount, DateTime date, string note)
+    {
+
+    }
+
+    public void MakeWithdrawal(decimal amount, DateTime date, string note)
+    {
+
+    }
+}
