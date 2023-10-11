f1aad5759fc31c18ba1bf928885b845a9a3886d6
sadik312 <sadikislam46@gmail.com>
2023-10-11 22:05:54 +0100

    creating Transaction.cs to handle deposits and withdrawals - will be tracked

--- cs_fundamentals/bankapp/Transaction.cs
+++ cs_fundamentals/bankapp/Transaction.cs
@@ -3,7 +3,17 @@
 
 namespace Classes;
 
-public class transaction
+public class Transaction
 {
+    // Fields - data members of a class or struct that store values or data associated with instances of that class or struct
+    public decimal Amount { get; set; }
+    public DateTime Date { get; set; }
+    public string Notes { get; set; }
 
+    public Transaction(decimal amount, DateTime date, string note)
+    {
+        Amount = amount;
+        Date = date;
+        Notes = note;
+    }
 }
