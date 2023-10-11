05775a9c5fa3d57d3a047bbfecb6c6b3c310e3b1
sadik312 <sadikislam46@gmail.com>
2023-10-11 13:37:28 +0100

    added private data member and initialised it

--- cs_fundamentals/bankapp/BankAccount.cs
+++ cs_fundamentals/bankapp/BankAccount.cs
@@ -2,6 +2,7 @@ namespace Classes;
 
 public class BankAccount
 {
+    private static int s_accountNumberSeed = 1234567890; // private data member 
     public string Number { get; set; }
     public string Owner { get; set; }
     public decimal Balance { get; set; }
@@ -10,6 +11,8 @@ public class BankAccount
     {
         this.Owner = name; // obj is constructed using this qualifier - it is optional
         this.Balance = initialBalance;
+        Number = s_accountNumberSeed.ToString();
+        s_accountNumberSeed++;
     }
     public void MakeDeposit(decimal amount, DateTime date, string note)
     {
